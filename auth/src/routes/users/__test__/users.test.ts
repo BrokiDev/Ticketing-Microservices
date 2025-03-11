a8aae4d7170e797ef2d1dae6a2b9ba13caa4a12d
BrokiDev <bryantramirez855@gmail.com>
2025-03-11 19:06:43 -0400

    refactor user test for currentUser endpoint; improve cookie handling and ensure proper response validation

--- auth/src/routes/users/__test__/users.test.ts
+++ auth/src/routes/users/__test__/users.test.ts
@@ -4,24 +4,21 @@ import app from "../../../app"
 
 
 it("return a 200 on currentUser",async() => {
-     const response = await request(app).post("/api/auth/signup").send({
+    const response = await request(app).post("/api/auth/signup").send({
         name: "broki",
         lastName: "Test",
-        email:"broki@test.com",
-        password:"1234"
+        email: "broki@test.com",
+        password: "1234"
     }).expect(201);
 
-    const cookie = response.get("Set-Cookie");
+    const cookies = response.get("Set-Cookie");
+    const cookie = cookies?.toString().split(";")[0];
 
-    if(!cookie) {
+    if (!cookie) {
         throw new Error("Cookie not found");
     }
 
-
-    const currentUser = await request(app).get("/api/users/currentUser").set("Cookie",cookie);
-
-    console.log(currentUser.get("Cookie"));
-
+    const currentUser = await request(app).get("/api/users/currentUser").set("Cookie", cookie).expect(200);
     expect(currentUser.body.data.user.email).toEqual("broki@test.com");
 });
 
