b0bf84185fce1bab96d3c2dfd5b05bb388fb3f61
BrokiDev <bryantramirez855@gmail.com>
2025-04-06 22:47:24 -0400

    refactor useRequest hook; improve type definitions for headers and clean up request structure

--- client/src/app/hooks/useRequest.ts
+++ client/src/app/hooks/useRequest.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useState } from "react";
 
 interface UseRequestProps {
   method: "get" | "post" | "put" | "delete";
     URL: string;
     body?: any;
-    headers?: any;
+    headers?: AxiosRequestConfig<any> | undefined;
 }
 
 
@@ -16,9 +16,11 @@ const useRequest = ({method = "get",URL,body,headers}:UseRequestProps) => {
     
     const makeRequest = async () => {
         try {
-        const response = await axios[method](URL, body, {
-            ...headers
-        } );
+        const response = await axios[method](URL, body,{
+            ...headers,
+        });
+
+        axios.post("",{},)
         setData(response.data);
         } catch (error:any) {
         setErrors(error.response.data.errors);
