5af0dc142a89ad29f9a4c84e310750ed2ef5e4b6
BrokiDev <bryantramirez855@gmail.com>
2025-04-12 14:09:51 -0400

    initial commit

---
+++ common/src/middlewares/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { Request,Response,NextFunction } from "express";
+import { JWT_Service } from "../services/jwt.service";
+import { BadRequestError } from "../../../common/src/errors/bad-request-error";
+
+export interface IUser {
+  id: string;
+  name: string
+  lastName:string
+  email:string
+  password:string
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: IUser;
+    }
+  }
+}
+
+
+export const authMiddleware = async (req:Request,res:Response,next:NextFunction) => {
+  const session_token = req.headers.cookie?.split("=")[1];
+
+  const validToken = JWT_Service.verifyToken(session_token as string);
+
+  if (!validToken) {
+    return next(new BadRequestError("You don't have access to this route"));
+  }
+
+  req.user = validToken as IUser;
+
+  next();
+}
---
+++ common/src/services/jwt.service.ts
@@ -0,0 +1,28 @@
+
+import { config } from 'dotenv';
+import { Response } from 'express';
+import {sign,verify,decode, JwtPayload} from 'jsonwebtoken'
+config({
+    path: '.env'
+})
+
+const JWT_KEY = process.env.JWT_KEY ?? ""
+const JWT_EXPIRES = process.env.JWT_EXPIRES_IN ?? "1d"
+
+
+export class JWT_Service {
+    static async generateToken(payload:object,res:Response) {
+        const token = sign(payload,JWT_KEY, {
+            expiresIn: "1d",
+        })
+
+        return res.cookie('session_token',token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        })
+    }
+    static verifyToken(token:string):JwtPayload&{id:string,email:string}  {
+        return verify(token,JWT_KEY || "") as JwtPayload&{id:string,email:string};
+    }
+}
