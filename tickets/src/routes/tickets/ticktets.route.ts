d90133db46ecc97bf9dd0100003ddb6959adb110
BrokiDev <bryantramirez855@gmail.com>
2025-04-17 14:50:23 -0400

    Implement ticket controllers and update routes for CRUD operations

--- tickets/src/routes/tickets/ticktets.route.ts
+++ tickets/src/routes/tickets/ticktets.route.ts
@@ -1,33 +1,16 @@
 import Router  from "express";
 import { authMiddleware} from "@bticket/common"
+import { getAllTicketsController,createTicketsController,deleteOneTicketsController,getOneTicketsController,updateOneTicketsController } from "../../controllers/tickets";
 
 const ticketsRouter = Router();
 
 
-ticketsRouter.get("/tickets",authMiddleware,(req,res) => {
-    res.status(200).json({
-        status:"pending",
-        message:"route not implemented yet"
-    })
-});
-ticketsRouter.get("/tickets/:id",(req,res) => {
-    res.status(200).json({
-        status:"pending",
-        message:"route not implemented yet"
-    })
-});
-ticketsRouter.post("/tickets",(req,res) => {
-    res.status(201).json({
-        status:"pending",
-        message:"route not implemented yet"
-    })
-});
-ticketsRouter.patch("/tickets/:id",(req,res) => {
-    res.status(200).json({
-        status:"pending",
-        message:"route not implemented yet"
-    })
-});
+ticketsRouter.get("/tickets",authMiddleware,getAllTicketsController);
+ticketsRouter.get("/tickets/:id",getOneTicketsController);
+ticketsRouter.post("/tickets",createTicketsController);
+ticketsRouter.patch("/tickets/:id",updateOneTicketsController);
+
+ticketsRouter.delete("/tickets/:id",deleteOneTicketsController);
 
 
 export default ticketsRouter;
